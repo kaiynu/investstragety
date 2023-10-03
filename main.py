58175be563eea501aa01d2812d2f972df11f6105
Kaiynu <kaileeynu@gmail.com>
2023-10-03 10:50:38 +0800

    盈增亏缩，沪深300轮动策略
    盈增亏缩，沪深300轮动策略

---
+++ main.py
@@ -0,0 +1,42 @@
+# 导入函数库
+from jqdata import *
+import json
+
+# 初始化函数，设定基准等等
+def initialize(context):
+    # 设定沪深300作为基准
+    set_benchmark('000300.XSHG')
+    # 开启动态复权模式(真实价格)
+    set_option('use_real_price', True)
+    # 输出内容到日志 log.info()
+    log.info('初始函数开始运行且全局只运行一次')
+    # 过滤掉order系列API产生的比error级别低的log
+    # log.set_level('order', 'error')
+
+    ### 股票相关设定 ###
+    # 股票类每笔交易时的手续费是：买入时佣金万分之三，卖出时佣金万分之三加千分之一印花税, 每笔交易佣金最低扣5块钱
+    set_order_cost(OrderCost(close_tax=0.001, open_commission=0.0003, close_commission=0.0003, min_commission=5), type='stock')
+
+    ## 运行函数（reference_security为运行时间的参考标的；传入的标的只做种类区分，因此传入'000300.XSHG'或'510300.XSHG'是一样的）
+      # 开盘前运行
+    run_daily(before_market_open, time='before_open', reference_security='000300.XSHG')
+      # 开盘时运行
+    run_daily(market_open, time='open', reference_security='000300.XSHG')
+      # 收盘后运行
+    run_daily(after_market_close, time='after_close', reference_security='000300.XSHG')
+
+## 开盘前运行函数
+def before_market_open(context):
+    content = read_file('data.json')
+    securities = eval(content)
+    log.info(securities)
+    pass
+
+## 开盘时运行函数
+def market_open(context):
+
+    pass
+
+## 收盘后运行函数
+def after_market_close(context):
+   pass
